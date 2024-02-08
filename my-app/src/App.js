fd9551da79d6700dc642e9d76822a2e58c4de6e2
Zolnikov Leonid <leonid211298@gmail.com>
2024-02-08 08:50:58 +0500

    without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,36 @@
 import logo from "./logo.svg"; //императивный
 import "./App.css"; // императивный
+import React from "react"; // императивный
 
 function App() {
-	//
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>{new Date().getFullYear()}</span>
-			</header>
-		</div>
+	const AppElement = React.createElement(
+		"div",
+		{ className: "App" },
+		React.createElement(
+			"header",
+			{ className: "App-header" },
+			React.createElement("img", {
+				className: "App-logo",
+				alt: "logo",
+				src: logo,
+			}),
+			React.createElement("p", {}, `Edit src/App.js and save to reload.`),
+			React.createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			React.createElement("span", {}, `${new Date().getFullYear()}`)
+		)
 	);
-} // декларативный
+
+	console.log(AppElement);
+	return AppElement;
+} // императивный
 
 export default App; //императивный
